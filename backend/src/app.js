ae7acd56b2e96d32a623e142074485a414bacad3
bobby.liu <fellowe5@hotmail.com>
2024-01-15 18:32:38 +0800

    Adding more features

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,12 @@ import {
   deleteRepairService,
   addRepairService,
   getRealtimeRepairServices,
+  getRealtimeFilteredRepairServices,
+  getSingleDocument,
 } from "./repairServiceActions";
 
 // firestore app
-import { getFirestore, collection } from "firebase/firestore";
+import { getFirestore, collection, doc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8wikOrixKqbm2lSW-unHcOcMlNWVUsGw",
@@ -60,4 +62,45 @@ deleteForm.addEventListener("submit", (e) => {
   deleteForm.reset();
 });
 
+const updateForm = document.querySelector(".update");
+const updateFormValidation = document.querySelector(".updateFormValidationMsg");
+updateForm.addEventListener("submit", (e) => {
+  const docID = updateForm.id.value;
+  const repairName = updateForm.updateRepairName.value;
+  const repairPrice = updateForm.updateRepairPrice.value;
+
+  e.preventDefault();
+  if (
+    //both repair name and repair price are empty
+    repairName === "" &&
+    repairPrice === ""
+  ) {
+    updateFormValidation.innerHTML =
+      "At least one field is required. Try again.";
+    updateFormValidation.style.color = "red";
+    updateFormValidation.style.textAlign = "center";
+
+    setTimeout(() => (updateFormValidation.innerHTML = ""), 3000);
+    console.log("Try again");
+  } else {
+    const docRef = doc(repairServiceCollection, docID);
+    //both repair name and repair price are empty
+    if (repairName === "" && repairPrice !== "") {
+      updateDoc(docRef, { price: Number(repairPrice) }).then(() =>
+        updateForm.reset()
+      );
+    }
+
+    if (repairName !== "" && repairPrice === "") {
+      updateDoc(docRef, { name: repairName }).then(() => updateForm.reset());
+    }
+  }
+});
+
 getRealtimeRepairServices(repairServiceCollection);
+// getRealtimeFilteredRepairServices(
+//   repairServiceCollection,
+//   "name",
+//   "handy-work"
+// );
+// getSingleDocument(repairServiceCollection, "KFHsBkdainQXyOrMsJhz");
--- backend/src/repairServiceActions.js
+++ backend/src/repairServiceActions.js
@@ -3,6 +3,9 @@ import {
   onSnapshot, //get data from firestore in realtime
   deleteDoc,
   doc, //reference to the document we want to delete
+  query,
+  where, //used in combination to get filtered data from firestore
+  getDoc,
 } from "firebase/firestore";
 
 export const deleteRepairService = async (_database, _collection, _docID) => {
@@ -14,10 +17,10 @@ export const deleteRepairService = async (_database, _collection, _docID) => {
 
   try {
     // get reference of the document to delete
-    const docref = doc(_database, _collection, _docID);
+    const docRef = doc(_database, _collection, _docID);
 
     // delete the document
-    await deleteDoc(docref);
+    await deleteDoc(docRef);
 
     // if deletion is successfully
     return true;
@@ -66,4 +69,52 @@ export const getRealtimeRepairServices = (_collection) => {
       console.error(error);
     }
   });
+};
+
+export const getRealtimeFilteredRepairServices = (
+  _collection,
+  _docKey,
+  _docValue
+) => {
+  /*
+    @_collection: value of collection
+    @_docKey: value of the property key we are filtering 
+    @_docValue: value we are filtering
+  */
+
+  const filterCollection = query(_collection, where(_docKey, "==", _docValue));
+
+  // get documents from collection in real-time and store in snapshots
+  onSnapshot(filterCollection, (snapshot) => {
+    try {
+      const repairServicesList = [];
+      // add document data, document id to list of dictionary
+      snapshot.docs.forEach((doc) => {
+        repairServicesList.push({
+          ...doc.data(),
+          id: doc.id,
+        });
+      });
+      console.log(repairServicesList);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
+
+export const getSingleDocument = async (_collection, _docID) => {
+  /*
+    @_collection: value of collection
+    @_docID: value of document id to delete
+  */
+  try {
+    // get reference of the document to delete
+    const docRef = doc(_collection, _docID);
+    const docu = await getDoc(docRef);
+    const docuData = { ...docu.data(), id: docu.id };
+    console.log(docuData);
+  } catch (e) {
+    console.error(`getSingleDocument: Something went wrong\n${e}`);
+    return false;
+  }
 };
