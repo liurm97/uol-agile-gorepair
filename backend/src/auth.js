f4df89284f53958fed3bf91730f7603ac548d23d
bobby.liu <fellowe5@hotmail.com>
2024-02-05 22:33:16 +0800

    removed unnecessary backend/ folder

--- backend/src/auth.js
+++
@@ -1,9 +0,0 @@
-// import { auth } from "firebase/auth";
-
-console.log("Sign up");
-const signUpForm = document.querySelector(".signup");
-
-signUpForm.addEventListener("submit", (e) => {
-  console.log("sign up form");
-  e.preventDefault();
-});
