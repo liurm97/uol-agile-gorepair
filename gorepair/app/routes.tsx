82f565e678667db2fa3d922684da2ca81c488a0e
bobby.liu <fellowe5@hotmail.com>
2024-03-04 00:14:34 +0800

    fix deployment attempt #12

---
+++ gorepair/app/routes.tsx
@@ -0,0 +1,42 @@
+import { Icon } from "@chakra-ui/react";
+import {
+  MdBarChart,
+  MdPerson,
+  MdHome,
+  MdLock,
+  MdOutlineShoppingCart,
+} from "react-icons/md";
+
+// Auth Imports
+// import SignInCentered from './pages/auth/sign-in';
+import { IRoute } from "./types/navigation";
+
+const routes: IRoute[] = [
+  //Admin
+  {
+    name: "🛡️Orders",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: "/orders",
+  },
+  {
+    name: "🛡️Users",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: "/users",
+  },
+  {
+    name: "🛡️Services",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: "/services",
+  },
+  {
+    name: "🛡️Profile",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+  },
+];
+
+export default routes;
