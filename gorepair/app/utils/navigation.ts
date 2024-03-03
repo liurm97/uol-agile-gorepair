a62e8d89b4081fd69a850e5dbdb8de3b636cd3ea
bobby.liu <fellowe5@hotmail.com>
2024-03-04 00:07:59 +0800

    fix deployment attempt #10

--- gorepair/app/utils/navigation.ts
+++ gorepair/app/utils/navigation.ts
@@ -1,10 +1,10 @@
-import { IRoute } from "types/navigation";
+import { IRoute } from "../types/navigation";
 
 // NextJS Requirement
 export const isWindowAvailable = () => typeof window !== "undefined";
 
-export const findCurrentRoute = (routes: IRoute[]): IRoute => { 
-  const foundRoute: IRoute = routes.find(
+export const findCurrentRoute = (routes: IRoute[]): IRoute | undefined => {
+  const foundRoute: IRoute | undefined = routes.find(
     (route) =>
       isWindowAvailable() &&
       window.location.href.indexOf(route.layout + route.path) !== -1 &&
@@ -19,7 +19,7 @@ export const getActiveRoute = (routes: IRoute[]): string => {
   return route?.name || "Default Brand Text";
 };
 
-export const getActiveNavbar = (routes: IRoute[]): boolean => {
+export const getActiveNavbar = (routes: IRoute[]): boolean | undefined => {
   const route = findCurrentRoute(routes);
   return route?.secondary;
 };
